3f36a72a4b66ad5a24f3984ec7a11efdd2427041
joestarr <takamoriuchi27@gmail.com>
2023-04-13 15:21:47 +0700

    add component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Browse from "./components/Browse";
+import Arrived from "./components/Arrived";
+import Clients from "./components/Clients";
+import AsideMenu from "./components/AsideMenu";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='text-3x1 font-bold underline'>
-        Hello people!
-      </h1>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Hero/>
+            <Browse/>
+            <Arrived/>
+            <Clients/>
+            <AsideMenu/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
